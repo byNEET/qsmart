370dc115c3fbeafff730b79bdd71f40657ed3b9e
dickyrey <dicky.rey97@gmail.com>
2019-12-16 10:13:39 +0700

    beta tester

--- lib/src/data/staticdata.dart
+++ lib/src/data/staticdata.dart
@@ -1,5 +1,5 @@
-class StaticData{
-  final List<String> kelas=[
+class StaticData {
+  final List<String> kelas = [
     "kelas 1",
     "kelas 2",
     "kelas 3",
@@ -12,14 +12,14 @@ class StaticData{
     "kelas 10",
   ];
 
-  final Map<String,List<String>> kelasV2={
-    "SD": ["kelas 1","kelas 2","kelas 3","kelas 4","kelas 5","kelas 6"],
-    "SMP": ["kelas 7","kelas 8","kelas 9"],
-    "SMA-IPA": ["kelas 10 IPA","kelas 11 IPA","kelas 12 IPA"],
-    "SMA-IPS": ["kelas 10 IPS","kelas 11 IPS","kelas 12 IPS"],
+  final Map<String, List<String>> kelasV2 = {
+    "SD": ["kelas 1", "kelas 2", "kelas 3", "kelas 4", "kelas 5", "kelas 6"],
+    "SMP": ["kelas 7", "kelas 8", "kelas 9"],
+    "SMA-IPA": ["kelas 10 IPA", "kelas 11 IPA", "kelas 12 IPA"],
+    "SMA-IPS": ["kelas 10 IPS", "kelas 11 IPS", "kelas 12 IPS"],
   };
 
-  final List<String> mapel=[
+  final List<String> mapel = [
     "Matematika",
     "Fisika",
     "Kimia",
@@ -27,17 +27,12 @@ class StaticData{
     "Bahasa Ingris"
   ];
 
-  final Map<String,List<String>> mapelV2 ={
-    "SD": ["Matematika","Tematik"],
-    "SMP": ["Bhs Ingris","Matematika","IPA"],
-    "SMA-IPA": ["Bhs Ingris","Matematika","Kimia","Fisika"],
-    "SMA-IPS": ["Bhs Ingris","Matematika","Akuntansi","Ekonomi"]
+  final Map<String, List<String>> mapelV2 = {
+    "SD": ["Matematika", "Tematik"],
+    "SMP": ["Bhs Ingris", "Matematika", "IPA"],
+    "SMA-IPA": ["Bhs Ingris", "Matematika", "Kimia", "Fisika"],
+    "SMA-IPS": ["Bhs Ingris", "Matematika", "Akuntansi", "Ekonomi"]
   };
 
-  final List<String> jenis=[
-    "Biase",
-    "TryOut",
-    "UN",
-    "UTBK"
-  ];
+  final List<String> jenis = ["Biasa", "TryOut", "UN", "UTBK"];
 }
